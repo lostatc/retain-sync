0d93e63d8f7c313ad0b16207082d5afdbbbe88aa
Garrett Powell <garrett@gpowell.net>
2016-12-26 11:30:39 -0500

    Add pre-commit hook for generating html man page

---
+++ CONTRIBUTING.md
@@ -0,0 +1,13 @@
+####Hooks
+Run the following command in the cloned git repository to include client-side
+hooks.
+```
+git config --local core.hooksPath ".hooks"
+```
+
+####Development Dependencies
+* beautifulsoup
+* lxml
+
+####Style
+This project follows the style guidelines outlined in PEP8.
---
+++ scripts/man-format.py
@@ -0,0 +1,31 @@
+"""Add formatting to an html man page."""
+
+import re
+import sys
+import os
+
+from bs4 import BeautifulSoup
+
+in_file = sys.argv[1]
+if not os.path.isfile(in_file):
+    print("Error: argument must be a file")
+    sys.exit(1)
+
+with open(in_file) as man_file:
+    soup = BeautifulSoup(man_file, "lxml")
+
+# Link external CSS stylesheet.
+soup.head.append(soup.new_tag(
+    "link", rel="stylesheet", type="text/css", href="man.css"))
+
+# Wrap heading hyperlinks with a CSS class.
+first_link = soup.find("a", href=re.compile("^#"))
+nav_tag = soup.new_tag("div")
+nav_tag["class"] = "man-navigation"
+first_link.insert_before(nav_tag)
+for link in [first_link] + first_link.find_next_siblings(["a", "br"]):
+    link.extract()
+    nav_tag.append(link)
+
+with open(in_file, "w") as man_file:
+    man_file.write(soup.prettify())
