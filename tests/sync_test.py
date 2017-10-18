5715f27eaf0991130a9da4706f49dbda100cd714
Garrett Powell <garrett@gpowell.net>
2017-10-18 12:15:57 -0400

    Add tests for "sync" command
    The purpose of these tests is to test that options in the config file
    modify the syncing behavior as expected.

--- tests/sync_test.py
+++ tests/sync_test.py
@@ -25,7 +25,6 @@ import textwrap
 
 import pytest
 
-from zielen.io import rec_scan
 from zielen.commands.sync import SyncCommand
 from zielen.commands.init import InitCommand
 
@@ -140,7 +139,7 @@ def test_new_remote_files_are_synced(command):
 def test_new_remote_files_are_symlinked(command):
     """New remote files are symlinked if there's not enough space."""
     with open("remote/letters/upper/B.txt", "w") as file:
-        file.write("B"*BLOCK_SIZE*4)
+        file.write("B"*BLOCK_SIZE*5)
     command.main()
 
     local_paths = set(command.local_dir.scan_paths(
@@ -224,14 +223,14 @@ def test_conflict_file_is_created(command):
 
 def test_files_are_prioritized(command):
     """Files and directories are prioritized based on their usage and size."""
-    os.makedirs("local/symbols")
+    time.sleep(0.1)
     with open("local/letters/a.txt", "w") as file:
         file.write("a"*BLOCK_SIZE*3)
     with open("local/letters/upper/A.txt", "w") as file:
         file.write("A"*BLOCK_SIZE*4)
     with open("local/numbers/1.txt", "w") as file:
         file.write("1"*BLOCK_SIZE*7)
-    with open("local/symbols/_.txt", "w") as file:
+    with open("local/_.txt", "w") as file:
         file.write("_"*BLOCK_SIZE*1)
     command.main()
 
@@ -242,7 +241,7 @@ def test_files_are_prioritized(command):
     expected_paths = {
         "letters/a.txt",
         "letters/upper/A.txt",
-        "symbols/_.txt"}
+        "_.txt"}
     expected_symlink_paths = {
         "numbers/1.txt"}
     assert local_paths == expected_paths
@@ -340,3 +339,75 @@ def test_priority_of_new_files_is_inflated(command):
     command.main()
     
     assert command.profile.get_path_info("letters/upper/B.txt").priority == 3.0
+
+
+def test_option_use_trash(command):
+    """The config option 'UseTrash' works as expected."""
+    os.remove("local/letters/a.txt")
+    with open(command.profile.cfg_path, "a") as file:
+        file.write("UseTrash=no\n")
+    command.main()
+
+    assert not list(os.scandir(command.dest_dir.trash_dir))
+
+
+def test_option_sync_extra_files(command):
+    """The config option 'SyncExtraFiles' works as expected."""
+    time.sleep(0.1)
+    with open("local/letters/a.txt", "w") as file:
+        file.write("a"*BLOCK_SIZE*3)
+    with open("local/letters/upper/A.txt", "w") as file:
+        file.write("A"*BLOCK_SIZE*4)
+    with open("local/numbers/1.txt", "w") as file:
+        file.write("1"*BLOCK_SIZE*7)
+    with open("local/_.txt", "w") as file:
+        file.write("_"*BLOCK_SIZE*1)
+    with open(command.profile.cfg_path, "a") as file:
+        file.write("SyncExtraFiles=no\n")
+    command.main()
+
+    local_paths = set(command.local_dir.scan_paths(
+        memoize=False, symlinks=False, dirs=False).keys())
+    local_symlink_paths = set(command.local_dir.scan_paths(
+        memoize=False, files=False, dirs=False).keys())
+    expected_paths = {
+        "letters/a.txt",
+        "letters/upper/A.txt"}
+    expected_symlink_paths = {
+        "numbers/1.txt",
+        "_.txt"}
+    assert local_paths == expected_paths
+    assert local_symlink_paths == expected_symlink_paths
+
+
+def test_option_inflate_priority(command):
+    """The config option 'InflatePriority' works as expected."""
+    command.profile.increment(["numbers/1.txt"], 2)
+    with open("local/letters/upper/B.txt", "w") as file:
+        file.write("B"*BLOCK_SIZE*2)
+    with open(command.profile.cfg_path, "a") as file:
+        file.write("InflatePriority=no\n")
+    command.main()
+
+    assert command.profile.get_path_info("letters/upper/B.txt").priority == 0.0
+
+
+def test_option_account_for_size(command):
+    """The config option 'AccountForSize' works as expected."""
+    time.sleep(0.1)
+    with open("local/numbers/1.txt", "w") as file:
+        file.write("1"*BLOCK_SIZE*8)
+    with open(command.profile.cfg_path, "a") as file:
+        file.write("AccountForSize=no\n")
+    command.profile.increment(["numbers/1.txt"], 2)
+    command.main()
+
+    local_paths = set(command.local_dir.scan_paths(
+        memoize=False, symlinks=False, dirs=False).keys())
+    local_symlink_paths = set(command.local_dir.scan_paths(
+        memoize=False, files=False, dirs=False).keys())
+    expected_symlink_paths = {
+        "letters/a.txt",
+        "letters/upper/A.txt"}
+    assert local_paths == {"numbers/1.txt"}
+    assert local_symlink_paths == expected_symlink_paths
